d2ededca9826ff902b4a4f5a9217ed362c2a0555
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2025-09-01 22:45:11 -0300

    feat(fretes): Inicio de tela

--- App/src/screens/private/Fretes.tsx
+++ App/src/screens/private/Fretes.tsx
@@ -1,12 +1,39 @@
-import { Text, View } from "react-native";
+import Constants from "expo-constants";
+import { useCallback, useEffect, useState } from "react";
+import { RefreshControl, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+
+const statusBarHeight = Constants.statusBarHeight;
 
 function Fretes() {
-  return (
-    <View className='flex-1 items-center justify-center'>
-      <Text>Frete</Text>
-    </View>
-  )
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(async () => {
+        setRefreshing(true);
+        setRefreshing(false);
+    }, []);
+
+    useEffect(() => {  
+    }, []);
+
+    return (
+        <SafeAreaView className='flex-1 items-center '>
+
+            <ScrollView
+                contentContainerStyle={{ paddingHorizontal: 6, marginTop: statusBarHeight + 10, paddingBottom: 130 }}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+                showsVerticalScrollIndicator={false}
+            >
+
+                <Text>Fretes Disponiveis</Text>
+
+            </ScrollView>
+
+
+        </SafeAreaView>
+
+    )
 }
 
 export default Fretes;
